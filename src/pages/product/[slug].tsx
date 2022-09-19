dc72e7d3c0c56c5f28c52a42db1f3d88d1c2847e
Renato Lomba <rntlomba@gmail.com>
2022-09-19 15:04:18 -0300

    first commit

---
+++ src/pages/product/[slug].tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const ProductPage: NextPage = () => {
+  return <h1>Product</h1>
+}
+
+export default ProductPage
---
+++ src/pages/success-purchase.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const SuccessPurchasePage: NextPage = () => {
+  return <h1>Success Purchase</h1>
+}
+
+export default SuccessPurchasePage
