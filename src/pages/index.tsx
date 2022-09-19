9e443a39aed771329768f0084f83cb6341ef554d
Renato Lomba <rntlomba@gmail.com>
2022-09-19 15:39:34 -0300

    stitches config with ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from 'next'
 
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  color: 'White',
+  border: 'none',
+  padding: '0.5rem',
+  borderRadius: 4,
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
+})
+
 const Home: NextPage = () => {
-  return <h1>Hello world</h1>
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Button>Teste</Button>
+    </div>
+  )
 }
 
 export default Home
